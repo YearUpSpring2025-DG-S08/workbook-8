a66aa6a2d9a29b453cd5471a5afe77b81b19de07
DamGarcia <damg883@gmail.com>
2025-06-10 10:06:15 -0400

    modifed code to display all product information from database

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -23,16 +24,35 @@ public class Main {
         // the statement is tied to the open connection
         Statement statement = connection.createStatement();
 
+        String categoryID;
+        
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("What category ID?");
+        
+        categoryID = s.nextLine();
+        
         // define your query
-        String query = "SELECT ProductName FROM products ";
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products WHERE categoryID = " + categoryID;
 
         // execute query
         ResultSet results = statement.executeQuery(query);
 
         // process the results
         while (results.next()){
-            String product = results.getString("ProductName");
-            System.out.println(product);
+            String productID = results.getString("ProductID");
+            String productName = results.getString("ProductName");
+            String UnitPrice = results.getString("UnitPrice");
+            String UnitsInStock = results.getString("UnitsInStock");
+
+
+            System.out.printf("""
+                    Product ID: %s
+                    Name: %s
+                    Price: $%.5s
+                    Stock: %s
+                    -------------------
+                    """, productID, productName, UnitPrice, UnitsInStock);
         }
         
         // close the connection
