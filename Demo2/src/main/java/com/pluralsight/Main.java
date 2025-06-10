dd73861f3f43d87e30f941969c7bf9c3a5dc8072
DamGarcia <damg883@gmail.com>
2025-06-10 10:04:54 -0400

    in-class try/catch/finally example

--- Demo2/src/main/java/com/pluralsight/Main.java
+++ Demo2/src/main/java/com/pluralsight/Main.java
@@ -4,36 +4,78 @@ package com.pluralsight;
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        Connection to the sakila database
-        // information to connect to the database
-        String connectionString = "jdbc:mysql://localhost:3306/sakila";
-        String username = "root";
-        String password = "yearup";
+    
+    private static sqlConnectionInfo sqlConnectionInfo;
+    public static void main(String[] args) {
+        
+        if (args.length != 3) {
+            System.out.println(
+                    "Application needs three arguments to run: " +
+                            "java.com.pluralsight.Main <username> <password> <sqlURL>");
+            System.exit(1);
+        }
+        
+        sqlConnectionInfo = getSQLConnectionInfoFromArgs(args);
+        
+        try{
+            displayCities(103);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static sqlConnectionInfo getSQLConnectionInfoFromArgs(String[] args){
+        // get username and password from the command line args
+        String username = args[0];
+        String password = args[1];
 
-        // load the MySQL driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionString = args[2];
+        
+        return new sqlConnectionInfo(connectionString, username, password);
+    }
+    
+    public static void displayCities(int countryID) throws SQLException {
+        // initialize the connection, statement, and results as null so they can be accessed within the catch and finally blocks
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet results = null;
+        
+        try {
+            // load the MySQL driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        // open a connection to the database
-        // use database URL to point to correct database
-        Connection connection;
-        connection = DriverManager.getConnection(connectionString, username, password);
+            // open a connection to the database
+            // use database URL to point to correct database
+            connection = DriverManager.getConnection(
+                    sqlConnectionInfo.getConnectionString(),
+                    sqlConnectionInfo.getUsername(),
+                    sqlConnectionInfo.getPassword());
 
-        // create statement
-        // the statement is tied to the open connection
-        Statement statement = connection.createStatement();
+            // define your query
+            String query = "SELECT city FROM city " +
+                    "WHERE country_id = ?;";
 
-        // define your query
-        String query = "SELECT city FROM city " +
-                "WHERE country_id = 103;";
+            // create statement
+            // the statement is tied to the open connection
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, countryID);
 
-        // execute query
-        ResultSet results = statement.executeQuery(query);
 
-        // process the results
-        while (results.next()){
-            String city = results.getString("city");
-            System.out.println(city);
+            // execute query
+            results = ps.executeQuery();
+            
+            // process the results
+            while (results.next()) {
+                String city = results.getString("city");
+                System.out.println(city);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (results != null) results.close();
+            if (ps != null) ps.close();
+            if(connection != null) connection.close();
         }
     }
 }
